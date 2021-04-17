8bf4aee4ef7a895628a43d1a3a35f1e2b81b66b3
dosbenjamin <dosbenjamin@gmail.com>
2021-04-17 18:19:13 +0200

    build(Tailwind): add colors and fonts configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'base': '1rem',
+        'lg': '1.5rem',
+        'xl': '2.25rem'
+      },
+      colors: {
+        white: '#FFF',
+        grey: '#AFAFAF',
+        blue: {
+          225: '#3A4047',
+          450: '#31353B',
+          775: '#2A2F36',
+          900: '#222831'
+        },
+        orange: '#F2A365',
+        red: '#F26565',
+        green: '#56C19C'
+      },
+      fontFamily: {
+        'mono': ['Inconsolata', 'monospace']
+      }
+    },
   },
   variants: {
     extend: {},
