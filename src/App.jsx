1178e1b77e822fc173ea7e29548b482c9eeca16d
dosbenjamin <dosbenjamin@gmail.com>
2021-04-24 10:05:14 +0200

    feat(Home): add application homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from '@/pages/Home'
 
 export default () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
+    <div className="flex items-center justify-center w-screen h-screen">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,31 @@
-import React from 'react'
+import React, { useState, useContext, useEffect, useReducer } from 'react'
+import RegisterSoundForm from '@/components/RegisterSoundForm/RegisterSoundForm'
+import Error, { ErrorContext } from '@/components/Error/Error'
 
-export default () => (<h1>Hello les amis !</h1>)
+export default () => {
+  const [errors, setErrors] = useState([])
+
+  return (
+    <div className="w-[1080px] flex">
+      <div className="w-1/2">
+        <div className="p-12 bg-blue-450">
+          <h1 className="max-w-[5em] text-xl leading-none">Jungle Soundbot</h1>
+        </div>
+      </div>
+      <div className="w-1/2 p-12 ml-2 bg-blue-225">
+        <ErrorContext.Provider value={setErrors}>
+          <RegisterSoundForm />
+        </ErrorContext.Provider>
+      </div>
+      <ul className="absolute w-[540px] bottom-4 left-1/2 transform -translate-x-1/2">
+        {errors.map((error, index) => (
+          <Error
+            message={error}
+            className={index > 0 && 'mt-2'}
+            key={error}
+          />
+        ))}
+      </ul>
+    </div>
+  )
+}
