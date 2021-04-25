5005490c9c4f68cde4ce9a2a7b6383b705366c4e
dosbenjamin <dosbenjamin@gmail.com>
2021-04-25 21:38:45 +0200

    feat(Button): add button hover animation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({ label, disabled = false, className = '', ...rest }) => (
   <>
     <button
-      className={`px-12 py-4 font-semibold text-blue-900 bg-orange disabled:bg-grey disabled:cursor-not-allowed ${
+      className={`px-12 py-4 font-semibold text-blue-900 focus:outline-none focus:border-blue-775 bg-orange-900 hover:bg-orange-450 transition-colors disabled:bg-grey disabled:cursor-not-allowed ${
         className
       }`}
       disabled={disabled}
