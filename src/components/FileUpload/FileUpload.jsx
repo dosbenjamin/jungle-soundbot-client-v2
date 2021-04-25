2965917cad62327ca582ca1467228dd83f095fbd
dosbenjamin <dosbenjamin@gmail.com>
2021-04-25 21:39:23 +0200

    feat(FileUpload): add drag and drop input to upload file

---
+++ src/components/FileUpload/FileUpload.jsx
@@ -0,0 +1,43 @@
+import React, { useReducer, useState, useRef } from 'react'
+import { useDropzone } from 'react-dropzone'
+import useInputBorder from '@/hooks/useInputBorder'
+
+export default ({ label, name, error, handler, className, defaultPlaceholder, dragPlaceholder }) => {
+  const [placeholder, setPlaceholder] = useState(defaultPlaceholder)
+  const dropzone = useRef(null)
+  const [loaded, setLoaded] = useState(false)
+  const { getRootProps, getInputProps, isDragActive, rootRef } = useDropzone()
+  const { borderColor, setCompleted, reset } = useInputBorder(error)
+
+  const changeHandler = ({ target }) => {
+    setLoaded(true)
+    setPlaceholder(target.files[0].name)
+    handler(target.value)
+    !!target.value.length ? setCompleted() : reset()
+    rootRef.current.blur()
+  }
+
+  return (
+    <div className={className}>
+      <label htmlFor={name}>{label}</label>
+      <div
+        className={
+          `p-12 bg-blue-775 cursor-pointer focus:border-grey focus:outline-none border text-center text-grey min-h-[160px] flex items-center justify-center mt-4 ${
+            borderColor
+        }`}
+        {...getRootProps()}
+      >
+        <input
+          {...getInputProps({ multiple: false })}
+          onChange={changeHandler}
+          id={name}
+          name={name}
+        />
+        {isDragActive
+          ? <p>{dragPlaceholder}</p>
+          : <p className={`max-w-[12em] leading-5 ${loaded ? 'text-white' : ''}`}>{placeholder}</p>
+        }
+      </div>
+    </div>
+  )
+}
