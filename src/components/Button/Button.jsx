bcb1d50127bd09b1a3d3ebc43f19ea25d5830311
dosbenjamin <dosbenjamin@gmail.com>
2021-05-01 17:26:30 +0200

    feat(Button): add possiblity to have children inside component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default ({ label, disabled = false, className = '', ...rest }) => (
+export default ({ children, label, disabled = false, className = '', ...rest }) => (
   <>
     <button
       className={`px-12 py-4 font-semibold text-blue-900 focus:outline-none focus:border-blue-775 bg-orange-900 hover:bg-orange-450 transition-colors disabled:bg-grey disabled:cursor-not-allowed ${
@@ -9,7 +9,7 @@ export default ({ label, disabled = false, className = '', ...rest }) => (
       disabled={disabled}
       {...rest}
     >
-      {label}
+      {children || label}
     </button>
   </>
 )
